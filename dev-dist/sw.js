7b4b705f69fc5534f325c599feada8e299db0e71
TarunShakya <shakyatarun32@gmail.com>
2026-05-13 22:06:19 +0530

    first commilllllpt

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.h4pfl8mkerg"
+    "revision": "0.7h3l2pa4he8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
